a295de2b877d21edfd684e8c91da1c3e6f9c73f5
huguoxing <guoxing.hu@weitaitec.com>
2019-04-17 10:32:48 +0800

    port:8081

--- config/index.js
+++ config/index.js
@@ -11,13 +11,13 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {    //需要代理的路径
-        target: 'http://127.0.0.1',  //需要代理的域名
+        target: 'http://127.0.0.1:8081',  //需要代理的域名
         changeOrigin: true, //必须配置为true，才能正确代理
       }
     },
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
